f0dc0177d04a3375a7763c83ad334ac102a3b93e
chayan <chayanchowdhury@gmail.com>
2024-11-14 23:18:33 +0530

    initial commit

---
+++ src/main/java/com/codeq/springboot_docker_compose/SpringbootDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package com.codeq.springboot_docker_compose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerComposeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeq/springboot_docker_compose/SpringbootDockerComposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeq.springboot_docker_compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerComposeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
