a5f28df906e11831e044d658294b1f492a04ad80
chayan <chayanchowdhury@gmail.com>
2024-11-15 10:39:57 +0530

    test cases added

--- src/main/java/com/codeq/springboot_docker_compose/SpringbootDockerComposeApplication.java
+++ src/main/java/com/codeq/springboot_docker_compose/SpringbootDockerComposeApplication.java
@@ -1,13 +1,22 @@
 package com.codeq.springboot_docker_compose;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootDockerComposeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDockerComposeApplication.class, args);
+	// Make logger static to access it in the static main method
+	private static final Logger logger = LoggerFactory.getLogger(SpringbootDockerComposeApplication.class);
+
+	public void init(){
+		logger.info("Application initialized");
 	}
 
+	public static void main(String[] args) {
+		logger.info("Application started");
+		SpringApplication.run(SpringbootDockerComposeApplication.class, args);
+	}
 }
--- src/test/java/com/codeq/springboot_docker_compose/SpringbootDockerComposeApplicationTests.java
+++ src/test/java/com/codeq/springboot_docker_compose/SpringbootDockerComposeApplicationTests.java
@@ -1,13 +1,21 @@
 package com.codeq.springboot_docker_compose;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootDockerComposeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static final Logger logger = LoggerFactory.getLogger(SpringbootDockerComposeApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+        logger.info("Test cases executing");
+        assertEquals(true, true);
+    }
 
 }
